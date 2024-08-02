247ecdc098495d0014f7b12258e421c36cd59d31
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-01 20:20:27 -0500

    add method to map Jikan API response adapted to Anime model

--- src/main/java/com/apime/service/AnimeService.java
+++ src/main/java/com/apime/service/AnimeService.java
@@ -1,10 +1,24 @@
 package com.apime.service;
 
+import com.apime.model.Anime;
+import com.apime.model.JikanAnime;
 import org.springframework.web.client.RestTemplate;
 @Service
 public class AnimeService {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String BASE_URL = "https://api.jikan.moe/v4";
+
+    private Anime mapToAnime(JikanAnime jikanAnime) {
+        Anime anime = new Anime();
+        anime.setMalId(jikanAnime.getMal_id());
+        anime.setTitle(jikanAnime.getTitle());
+        anime.setScore(jikanAnime.getScore());
+        anime.setRecommendation(getRecommendation(jikanAnime.getScore()));
+        anime.setImageUrl(jikanAnime.getImages().getJpg().getImage_url());
+        anime.setTrailerUrl(jikanAnime.getTrailer().getUrl());
+        anime.setYear(jikanAnime.getYear());
+        return anime;
+    }
 
     private String getRecommendation(double score) {
         if (score == 0) {
