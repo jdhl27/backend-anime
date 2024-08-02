bc38e1a177bea20e34e131acf2f1b9c70d6b1254
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-01 20:17:35 -0500

    add method to get recommendation text based on score

--- src/main/java/com/apime/service/AnimeService.java
+++ src/main/java/com/apime/service/AnimeService.java
@@ -5,4 +5,16 @@ import org.springframework.web.client.RestTemplate;
 public class AnimeService {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String BASE_URL = "https://api.jikan.moe/v4";
+
+    private String getRecommendation(double score) {
+        if (score == 0) {
+            return "--";
+        } else if (score <= 4) {
+            return "I do not recommend it.";
+        } else if (score <= 7) {
+            return "You may have fun.";
+        } else {
+            return "Great, this is one of the best anime.";
+        }
+    }
 }
