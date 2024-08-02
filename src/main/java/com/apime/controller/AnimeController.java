655c4deec7fc2e49b26e5ce39efd4d674d85eb8a
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-01 21:59:05 -0500

    delete seasonal average endpoint and modify search endpoint return

--- src/main/java/com/apime/controller/AnimeController.java
+++ src/main/java/com/apime/controller/AnimeController.java
@@ -1,13 +1,11 @@
 package com.apime.controller;
 
-import com.apime.model.Anime;
+import com.apime.model.AnimeResponse;
 import com.apime.service.AnimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class AnimeController {
@@ -15,12 +13,7 @@ public class AnimeController {
     private AnimeService animeService;
 
     @GetMapping("/search")
-    public List<Anime> searchAnime(@RequestParam String query) {
+    public AnimeResponse searchAnime(@RequestParam String query) {
         return animeService.searchAnime(query);
-    }
-
-    @GetMapping("/average-score")
-    public double getAverageScore(@RequestParam List<Anime> animes) {
-        return animeService.calculateAverageScore(animes);
     }
 }
