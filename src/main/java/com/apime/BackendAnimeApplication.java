3fff1ecb389333a28f8520e6a079c2c1cbafd413
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-01 20:07:23 -0500

    add project base with Spring Boot 3.3.2 dependencies

---
+++ src/main/java/com/apime/BackendAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.apime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apime/BackendAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
